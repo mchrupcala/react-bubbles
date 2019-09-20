df782f9b0258e97a1877a81ffcbe366379045add
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-09-20 11:37:13 -0400

    Added axiosWithAuth and PrivateRoute components. Answered the ReadMe questions. And put together the private route so users are redirected to login if they haven't yet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
 
 function App() {
   const [colorList, setColorList] = useState([]);
+
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+      <h1>Welcome to the Bubble App!</h1>
+        <Link to="/login">Login</Link>
+        <Link to="bubblepage">Dashboard</Link>
+        
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute path="/bubblepage" component={BubblePage}/>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import axiosWithAuth from "./axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState({});
+
+  const login = () => {
+    axiosWithAuth.post('http://localhost:5000/api/login', credentials)
+    .then(res => {
+      console.log(res.data);
+      localStorage.setItem('token', res.data.token);
+      props.history.push('/bubblepage');
+    })
+  }
+
+    const changeHandler = e => {
+      setCredentials({
+        ...credentials, 
+        [e.target.name]: e.target.value,
+      })
+    }
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <p>Built a login page here</p>
+
+      <div>
+
+        <form>
+          <input 
+          type="text" 
+          name="username" 
+          value={credentials.username}></input>
+
+          <input 
+          type="password" 
+          name="password" 
+          value={credentials.password}></input>
+
+          <button type="submit">Log In</button>
+
+        </form>
+      </div>
     </>
   );
 };
